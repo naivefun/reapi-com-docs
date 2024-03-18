e6bb7228bdaccd07f9bf41e8d9813165b5220340
PeiSong <peisong@outlook.sg>
2024-03-18 08:58:34 +0800

    feat: added analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
+import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
-import "../styles/global.css";
 import "reactflow/dist/style.css";
+import "../styles/global.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
